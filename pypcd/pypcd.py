f29adbcd0117f4d05c4301e732bcbc1c1a410979
Andreas Klintberg <andreas.klintberg@meltwater.com>
2018-03-20 23:36:28 -0700

    fixed py2 compatibility

--- pypcd/pypcd.py
+++ pypcd/pypcd.py
@@ -267,7 +267,7 @@ def point_cloud_from_fileobj(f):
     header = []
     while True:
         ln = f.readline().strip()
-        if isinstance(ln, bytes):
+        if not isinstance(ln, str):
             ln = ln.decode('utf-8')
         header.append(ln)
         if ln.startswith('DATA'):
